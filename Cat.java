827d0c747a2dbe24be659150f994d545717b8c24
ViaPlenos <148856857+ViaPlenos@users.noreply.github.com>
2023-10-24 17:09:49 +0800

    Cat.java

--- Cat.java
+++ Cat.java
@@ -1,21 +1,15 @@
-// ! Derived class
 class Cat extends Animal {
     String breed;
-
-    // ! Derived class constructor with super to initialize base class properties
-    public Dog(String name, String breed) {
-        super(name); // ! Call the base class constructor to initialize the 'name' property
+    public Cat(String name, String breed) {
+        super(name); 
         this.breed = breed;
     }
-
-    // ! Override the speak method of the base class
-    @Override
     public void speak() {
         System.out.println(name + " (a " + breed + " cat) meow.");
     }
 
-    // ! New method specific to the Cat class
+   
     public void fetch() {
-        System.out.println(name + " fetches a ball.");
+        System.out.println(name + " fetches a toy.");
     }
 }
